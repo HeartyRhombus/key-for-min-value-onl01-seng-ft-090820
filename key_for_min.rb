ab4335630f22bfe32358ce0ff5554d73d7aa5b6c
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-11 20:30:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+  name_hash.sort do |x, y|
+    x <=> y
+    name_hash.first[0]
+  end
+  
